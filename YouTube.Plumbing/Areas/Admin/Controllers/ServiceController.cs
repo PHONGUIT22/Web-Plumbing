208a45c13243cb77f505958117afd70c60df9b49
Your Name <your_email@example.com>
2024-08-06 20:51:15 +0700

    new Controller

---
+++ YouTube.Plumbing/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,53 @@
+﻿using EntityLayer.WebApplication.ViewModels.Service;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services.Abstract;
+
+namespace YouTube.Plumbing.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IServiceService _serviceService;
+
+        public ServiceController(IServiceService serviceService)
+        {
+            _serviceService = serviceService;
+        }
+
+        public async Task<IActionResult> GetServiceList()
+        {
+            var serviceList = await _serviceService.GetAllListAsync();
+            return View(serviceList);
+        }
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddService(ServiceAddVM request)
+        {
+            await _serviceService.AddServiceAsync(request);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var Service = await _serviceService.GetServiceById(id);
+            return View(Service);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(ServiceUpdateVM request)
+        {
+            await _serviceService.UpdateServiceAsync(request);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            await _serviceService.DeleteServiceAsync(id);
+            return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+        }
+    }
+}
