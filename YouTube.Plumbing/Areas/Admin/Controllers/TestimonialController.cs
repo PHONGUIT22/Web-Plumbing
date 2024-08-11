3cb5306f95895ca4ef31232835259eebb9920abf
Your Name <your_email@example.com>
2024-08-11 13:03:58 +0700

    A

---
+++ YouTube.Plumbing/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YouTube.Plumbing.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TestimonialController : Controller
+    {
+        
+    }
+}
