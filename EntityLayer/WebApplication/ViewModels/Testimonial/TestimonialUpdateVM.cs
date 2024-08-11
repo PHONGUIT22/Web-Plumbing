d7619ba6414fc879c4f55fb01566db987d0152a5
Your Name <your_email@example.com>
2024-08-11 13:44:27 +0700

    Change CRUD

--- EntityLayer/WebApplication/ViewModels/Testimonial/TestimonialUpdateVM.cs
+++ EntityLayer/WebApplication/ViewModels/Testimonial/TestimonialUpdateVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,5 +19,7 @@ namespace EntityLayer.WebApplication.ViewModels.Testimonial
         public string Title { get; set; } = null!;
         public string FileName { get; set; } = null!;
         public string FileType { get; set; } = null!;
+        public IFormFile Photo { get; set; } = null!;
+
     }
 }
--- YouTube.Plumbing/Areas/Admin/Controllers/TestimonialController.cs
+++ YouTube.Plumbing/Areas/Admin/Controllers/TestimonialController.cs
@@ -1,10 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.WebApplication.ViewModels.Testimonial;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services.Abstract;
 
 namespace YouTube.Plumbing.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class TestimonialController : Controller
     {
-        
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService TestimonialService)
+        {
+            _testimonialService = TestimonialService;
+        }
+
+        public async Task<IActionResult> GetTestimonialList()
+        {
+            var testimonialList = await _testimonialService.GetAllListAsync();
+            return View(testimonialList);
+        }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddTestimonial(TestimonialAddVM request)
+        {
+            await _testimonialService.AddTestimonialAsync(request);
+            return RedirectToAction("GetTestimonialList", "Testimonial", new { Area = ("Admin") });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var testimonial = await _testimonialService.GetTestimonialById(id);
+            return View(testimonial);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(TestimonialUpdateVM request)
+        {
+            await _testimonialService.UpdateTestimonialAsync(request);
+            return RedirectToAction("GetTestimonialList", "Testimonial", new { Area = ("Admin") });
+        }
+
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            await _testimonialService.DeleteTestimonialAsync(id);
+            return RedirectToAction("GetTestimonialList", "Testimonial", new { Area = ("Admin") });
+        }
     }
 }
